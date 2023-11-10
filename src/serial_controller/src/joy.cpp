89f385b14cbfed74f4d46ff2656018a34f8df76f
PauloPHAL <henriqueloss@hotmail.com>
2023-11-10 16:25:25 -0300

    Organizado

--- src/serial_controller/src/joy.cpp
+++ src/serial_controller/src/joy.cpp
@@ -8,6 +8,118 @@
 #include <termios.h>
 #include <unistd.h>
 /*
+rosparam set joy_node/dev "/dev/input/js0"
+rosrun joy joy_node
+source osorim_ws/devel/setup.bash
+
+Ao abrir o terminal primeira coisa deve se colocar o parametro de imput para setar o Joystick
+depois dar um source.... 
+igual os codigos acima
+*/
+
+//variaveis Statica
+serial::Serial arduino;
+int serial_port;
+
+
+void callback(const sensor_msgs::Joy::ConstPtr& msg) {
+  /*
+    nessa parte do codigo, setamos uma estrutura terminos, tty
+    e damos um serial_port = open, para abrir a comunicação com a porta serial
+  */
+  struct termios tty;
+  serial_port = open("/dev/ttyUSB0", O_RDWR);
+  if (serial_port < 0) {
+    perror("Erro ao abrir a porta serial");
+    return ;
+  }
+
+  /*
+    Aqui setamos os parametros da porta serial
+  */
+  memset(&tty, 0, sizeof(tty));
+  if (tcgetattr(serial_port, &tty) != 0) {
+    perror("Erro ao obter os atributos da porta serial");
+    return ;
+  }
+
+  /*
+    8 bits por caractere, habilita leitura, ignora controle de modem
+  */
+  tty.c_cflag = CS8 | CREAD | CLOCAL; 
+
+  /*
+    Ignora erros de paridade
+  */
+  tty.c_iflag = IGNPAR; 
+
+  /*
+    Modo não canônico
+  */
+  tty.c_oflag = 0;
+  tty.c_lflag = 0; 
+
+  /*
+    Define a velocidade de transmissão (9600 bps)
+  */
+  cfsetospeed(&tty, B9600); 
+
+  /*
+    Aplica as configurações à porta serial
+  */
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+    perror("Erro ao aplicar os atributos da porta serial");
+    return ;
+  }
+
+  /*
+    Ao passar por todas essas verificaçoes a porta serial esta aberta e configurada
+    corretamente para ser utilizada.
+    String a ser enviada
+  */  
+  char message;
+
+  /*
+    Leio atraves da "msg", q basicamente é uma classe do tipo Joy
+    passada por parametro na função callback
+    leio o vetor axes, que tem duas posiçoes [0,1]
+  */
+  if (msg->axes[1] == 1) {
+    message = 'W';
+  } else if (msg->axes[1] == -1) {
+    message = 'S';
+  } else if (msg->axes[0] == 1) {
+    message = 'A';
+  } else if (msg->axes[0] == -1) {
+    message = 'D';
+  }
+
+  // Envia a string para a porta serial 
+  printf("%c\n",message);
+  write(serial_port, &message, strlen(&message));
+    
+  // Fecha a porta serial
+  close(serial_port);
+
+}
+
+int main(int argc, char** argv) { 
+  ros::init(argc, argv, "joystick_control");
+
+  ros::NodeHandle nh;
+
+  ros::Subscriber joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy", 1, callback);
+
+  ros::spin();
+
+  return 0;
+}
+
+
+
+// Codigos Comentados
+
+/*
 void callback(const sensor_msgs::Joy::ConstPtr& msg) {
   
   //printf("Velocidade x: %.2f\n", msg->axes[1]);
@@ -24,101 +136,7 @@ void callback(const sensor_msgs::Joy::ConstPtr& msg) {
     printf("D\n");
   }
 }
-*/
 serial::Serial arduino;
-int serial_port;
-
-
-void callback(const sensor_msgs::Joy::ConstPtr& msg) {
-  
-  struct termios tty;
-// Abre a porta serial
-    serial_port = open("/dev/ttyUSB0", O_RDWR);
-    if (serial_port < 0) {
-        perror("Erro ao abrir a porta serial");
-        return ;
-    }
-
-    // Configura os parâmetros da porta serial
-    memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(serial_port, &tty) != 0) {
-        perror("Erro ao obter os atributos da porta serial");
-        return ;
-    }
-
-    tty.c_cflag = CS8 | CREAD | CLOCAL; // 8 bits por caractere, habilita leitura, ignora controle de modem
-    tty.c_iflag = IGNPAR; // Ignora erros de paridade
-    tty.c_oflag = 0;
-    tty.c_lflag = 0; // Modo não canônico
-    cfsetospeed(&tty, B9600); // Define a velocidade de transmissão (9600 bps)
-
-    // Aplica as configurações à porta serial
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-        perror("Erro ao aplicar os atributos da porta serial");
-        return ;
-    }
-  
-    
-    // String a ser enviada
-    char message;
-
-    if (msg->axes[1] == 1) {
-      message = 'W';
-    } else if (msg->axes[1] == -1) {
-      message = 'S';
-    } else if (msg->axes[0] == 1) {
-       message = 'A';
-    } else if (msg->axes[0] == -1) {
-      message = 'D';
-    }
-
-    
-
-
-    // Envia a string para a porta serial
-    
-    printf("%c\n",message);
-    write(serial_port, &message, strlen(&message));
-    
-    
-    
-
-    // Fecha a porta serial
-    close(serial_port);
-
-
-  
-  
-  
-}
-
-int main(int argc, char** argv) {
- 
-  ros::init(argc, argv, "joystick_control");
-  /*
-  arduino.setPort("/dev/ttyUSB0");  
-  arduino.setBaudrate(9600);
-  
-  try {
-      arduino.open();
-    } catch (serial::IOException& e) {
-      ROS_ERROR("Falha ao abrir a porta serial. Certifique-se de que o Arduino esteja conectado e configurado corretamente.");
-      return 1;
-    }*/
-
-  ros::NodeHandle nh;
-
-  ros::Subscriber joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy", 1, callback);
-
-  ros::spin();
-
-  return 0;
-}
-
-/*
-
-serial::Serial arduino;
-
 void callback(const sensor_msgs::Joy::ConstPtr& msg) {
   uint8_t command;
 
@@ -148,7 +166,6 @@ void callback(const sensor_msgs::Joy::ConstPtr& msg) {
   // Envie o comando para o Arduino via comunicação serial
   arduino.write(&command, 1);
 }
-
 int main(int argc, char** argv) {
   ros::init(argc, argv, "subiscriber");
   ros::NodeHandle nh;
@@ -168,12 +185,7 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
-*/
-/*
-
 serial::Serial arduino;
-
 void callback(const sensor_msgs::Joy::ConstPtr& msg) {
   uint8_t command;
   int d;
@@ -201,7 +213,6 @@ void callback(const sensor_msgs::Joy::ConstPtr& msg) {
   }
   
 }
-
 int main(int argc, char** argv) {
   ros::init(argc, argv, "joystick_control");
   ros::NodeHandle nh;
@@ -226,4 +237,5 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+Código comentado pq estava testando, foram tentativas falhas de comunicação com a porta serial
 */
